3a1891b9108dab151c2f1cac94b73fdaa421a49f
Busko Bogdan <busko.bogdan@gmail.com>
2018-10-13 19:57:05 +0300

    add account dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'dashboard#index'
 
   namespace :account do
+    get '/dashboard', to: 'dashboard#index'
     resources :projects do
       resources :tasks, except: :index
     end
